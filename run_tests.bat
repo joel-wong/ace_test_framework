08aff7dd5ea7f52925e70a1b4de813d9302d63c6
Joel Wong <joel.wong@ucalgary.ca>
2020-12-22 09:43:27 -0600

    Implement capabilities for starting tests through batch script, add manual options such as serial and part number

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,3 @@
-rem TODO: need to check if python 3 is installed here, and if not, then install it (note that "rem" is a comment)
-
-python robot\shared\suiterunner\setup_and_run_suites.py
+cd robot\shared\testmanager
+python TestManager.py
+cd ..\..\..\
