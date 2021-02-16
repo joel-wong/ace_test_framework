86fce8654f6fc61b41667ce1fe25a42922615547
Wenjia Yang <wendyyang0811@gmail.com>
2021-02-15 22:09:54 -0700

    PEP8 style (https://www.python.org/dev/peps/pep-0008/) requires that all lines are 79 characters or less unless there is a very good reason to have a longer line. For individual reports generated while running test suite continuously, timestamp reports with the UTC time

--- robot/shared/testmanager/TestManager.py
+++ robot/shared/testmanager/TestManager.py
@@ -1,8 +1,8 @@
 import datetime
-import json
 import os
 import subprocess
 import sys
+import copy
 
 from manual import MANUAL_TEST_CONSTANTS
 
@@ -31,20 +31,26 @@ class TestManager:
 
     def run_suites(self):
         suite_name = self.config_manager.get(ConfigManager.CONFIG_SUITE_NAME)
-        suite_directory = os.path.join(self.robot_directory, "suites", suite_name)
-        main_test_output_directory = os.path.join(self.base_directory, "out", suite_name)
+        suite_directory = os.path.join(self.robot_directory, "suites",
+                                       suite_name)
+        main_test_output_directory = os.path.join(self.base_directory, "out",
+                                                  suite_name)
         print("Starting tests now")
-        run_continuously = self.config_manager.get_bool(ConfigManager.CONFIG_REPEAT_TESTS)
+        run_continuously = self.config_manager.get_bool(
+            ConfigManager.CONFIG_REPEAT_TESTS)
 
-        output_directory = os.path.join(main_test_output_directory, TestManager.generate_datetime_directory_str())
+        output_directory = os.path.join(main_test_output_directory,
+                                        TestManager.generate_datetime_str())
         try:
             os.mkdir(output_directory)
         except FileExistsError:
             print("Error: Output directory name already exists. Exiting...")
             sys.exit(1)
-        individual_output_directory = os.path.join(output_directory, "INDIVIDUAL TESTS")
+        individual_output_directory = os.path.join(output_directory,
+                                                   "INDIVIDUAL TESTS")
 
-        config_file_output_name = os.path.join(output_directory, ConfigManager.CONFIG_FILE_NAME)
+        config_file_output_name = os.path.join(output_directory,
+                                               ConfigManager.CONFIG_FILE_NAME)
         self.config_manager.save_config(config_file_output_name)
 
         if run_continuously:
@@ -53,34 +59,43 @@ class TestManager:
             test_output_directory = output_directory
 
         subprocess_args = ["python", "-m", "robot", "--outputdir",
-                           test_output_directory, "--timestampoutputs"]
+                           test_output_directory]
 
-        include_manual_tests = self.config_manager.get_bool(ConfigManager.CONFIG_INCLUDE_MANUAL_TESTS)
+        include_manual_tests = self.config_manager.get_bool(
+            ConfigManager.CONFIG_INCLUDE_MANUAL_TESTS)
         if not include_manual_tests:
             subprocess_args.extend(
                 TestManager.generate_exclude_tags_subprocess_args(
                     [MANUAL_TEST_CONSTANTS.MANUAL_TEST_TAG]))
 
-        subprocess_args.append("{}/*.robot".format(suite_directory))
-
         if not run_continuously:
+            subprocess_args.append("{}/*.robot".format(suite_directory))
             subprocess.run(subprocess_args, shell=True, check=False)
             TestManager.print_tests_complete_message(output_directory)
             return
 
         try:
             while True:
-                subprocess.run(subprocess_args, shell=True, check=False)
+                current_run_args = copy.deepcopy(subprocess_args)
+                run_datetime = TestManager.generate_datetime_str()
+                current_run_args.extend(
+                    ["--output", "output-{}.xml".format(run_datetime),
+                     "--report", "report-{}.html".format(run_datetime),
+                     "--log", "log-{}.html".format(run_datetime)])
+                current_run_args.append("{}/*.robot".format(suite_directory))
+                subprocess.run(current_run_args, shell=True, check=False)
         except KeyboardInterrupt:
             print("Consolidating all test reports...")
-            merge_reports_subprocess_args = ["python", "-m", "robot.rebot",
-                                             "--outputdir", output_directory, "--name", "bnc_card",
-                                             "{}/*.xml".format(individual_output_directory)]
-            subprocess.run(merge_reports_subprocess_args, shell=True, check=False)
+            merge_reports_subprocess_args = [
+                "python", "-m", "robot.rebot", "--outputdir", output_directory,
+                "--name", "bnc_card",
+                "{}/*.xml".format(individual_output_directory)]
+            subprocess.run(merge_reports_subprocess_args, shell=True,
+                           check=False)
             TestManager.print_tests_complete_message(output_directory)
 
     @staticmethod
-    def generate_datetime_directory_str():
+    def generate_datetime_str():
         current_time = datetime.datetime.utcnow().isoformat("T")
         current_time = current_time.replace(":", "-").replace(".", "-")
         return current_time
@@ -99,11 +114,11 @@ class TestManager:
 
         :param tags_to_exclude: The list of tags associated with tests that
             will not be run
-        :return: The additional arguments to be passed to 'python -m robot' upon
-            startup to exclude the 'tags_to_exclude'
+        :return: The additional arguments to be passed to 'python -m robot'
+            upon startup to exclude the 'tags_to_exclude'
         """
-        # A current issue in robot framework is that tags are incorrectly parsed
-        # when they are variables.
+        # A current issue in robot framework is that tags are incorrectly
+        # parsed when they are variables.
         # This is one of several issues documented in
         # https://github.com/robotframework/robotframework/issues/3238
         #
