df543e1c6bfd91c88b0f53939c7ff11f59d70b9a
Benjamin Forseth <benjamin.forseth@ucalgary.ca>
2021-05-20 03:15:52 -0700

    Add batch number and serial number dir generation
    Changed result storage to search high level results dir for
    a dir named "BN<batch number>" and a subdirectory named
    "SN<Batch number>-<serial number>". If this dir does not exist, it will
    be created.

--- robot/shared/testmanager/TestManager.py
+++ robot/shared/testmanager/TestManager.py
@@ -29,6 +29,7 @@ class TestManager:
         self.config_manager = ConfigManager.ConfigManager(
             self.robot_directory, self.config_file_abspath)
 
+
         self.__in_gui_mode = in_gui_mode
         self.__gui = None
         self.robot_process = None
@@ -65,8 +66,8 @@ class TestManager:
         print("Starting tests now")
         run_continuously = self.config_manager.get_bool(
             ConfigManager.CONFIG_REPEAT_TESTS)
-
-        output_directory = os.path.join(main_test_output_directory,
+        results_parent_directory = self.get_results_directory(main_test_output_directory)
+        output_directory = os.path.join(results_parent_directory,
                                         TestManager.generate_datetime_str())
         try:
             os.makedirs(output_directory, mode=0o660)
@@ -185,6 +186,35 @@ class TestManager:
                        check=False)
         self.generate_excel_report(output_directory)
         self.print_tests_complete_message(output_directory)
+
+    def get_results_directory(self, high_level_directory):
+        """
+        Method that finds or creates required directories for storing test results
+
+        Takes high level directory, batch number, and serial number then stores results in the
+        form: high_level_dir/BN<batchNo>/SN<batchNo>-<serialNo>
+
+        :return: Path to results directory
+        """
+        batch_num_dir_name = "BN{}".format(self.config_manager.get(ConfigManager.CONFIG_BATCH_MO_NUMBER))
+        serial_num_dir_name = "SN{}-{}".format(self.config_manager.get(ConfigManager.CONFIG_BATCH_MO_NUMBER),
+                                               self.config_manager.get(ConfigManager.CONFIG_SERIAL_NUMBER))
+
+        dir_list = os.listdir(high_level_directory)
+        if batch_num_dir_name not in dir_list:
+            batch_num_dir_path = os.path.join(high_level_directory, batch_num_dir_name)
+            serial_num_dir_path = os.path.join(batch_num_dir_path, serial_num_dir_name)
+            os.mkdir(batch_num_dir_path)
+            os.mkdir(serial_num_dir_path)
+        else:
+            batch_num_dir_path = os.path.join(high_level_directory, batch_num_dir_name)
+            SN_dir_list = os.listdir(batch_num_dir_path)
+            if serial_num_dir_name not in SN_dir_list:
+                serial_num_dir_path = os.path.join(batch_num_dir_path, serial_num_dir_name)
+                os.mkdir(serial_num_dir_path)
+
+        results_path = os.path.join(high_level_directory, batch_num_dir_name, serial_num_dir_name)
+        return results_path
 
     def generate_excel_report(self, output_directory):
         self.xml_formatter = Xml2Excel(output_directory, output_directory, BATCH_SERIAL_FILENAME)
